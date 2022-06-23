78f4a9b8c567d5545992867d9d472169688e94c0
Railander Soares Alcantara dos Reis - TI Infraestrutura <railander.reis@redemeridional.com.br>
2022-06-23 12:29:56 -0300

    commit inicial

---
+++ simulator/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+	kafka2 "github.com/railandereis/simulator/application/kafka"
+	"github.com/railandereis/simulator/infra/kafka"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		go kafka2.Produce(msg)
+		fmt.Println(string(msg.Value))
+	}
+}
